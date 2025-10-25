231409e5795bc81243cbdec9b21a4807eba693d5
ARNAVSAROJ <178799881+ArnavSaroj@users.noreply.github.com.>
2025-10-26 00:15:06 +0530

    last change will improve it in future if time permits

--- main.cpp
+++ main.cpp
@@ -1,37 +1,36 @@
 #include <bits/stdc++.h>
-#include <queue>
 
 using namespace std;
 
-vector<vector<int>> inputGraph(int &num_rooms)
+vector<vector<int>> buildGraphStructure(int &total_nodes)
 {
 
-    cout << "--- Emergency Path Finder Program ---" << endl;
+    cout << "\n*** Building Evacuation Path Simulator ***\n";
     while (true)
     {
-        cout << "Enter the total number of rooms (nodes) in the building: ";
-        if (cin >> num_rooms && num_rooms > 1)
+        cout << "How many rooms are in your building layout? ";
+        if (cin >> total_nodes && total_nodes > 1)
         {
             break;
         }
-        cout << "Not valid format of input. Please enter a number greater than 1." << endl;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Not valid format of input. Please enter a number greater than 1.";
+        cin.sync();
+        cin.ignore(INT_MAX, '\n');
     }
 
-    vector<vector<int>> AM(num_rooms, vector<int>(num_rooms, 0));
+    vector<vector<int>> layout_Room(total_nodes, vector<int>(total_nodes, 0));
 
     cout << "\n--- Defining the Connections ---" << endl;
-    cout << "Enter room connections (edges). Rooms are numbered 0 to " << num_rooms - 1 << "." << endl;
+    cout << "Enter room connections (edges). Rooms are numbered 0 to " << total_nodes - 1 << "." << endl;
     cout << "Enter 'room_A room_B' to connect them (e.g., '0 1')." << endl;
     cout << "Enter '-1 -1' to finish adding connections." << endl;
 
-    int u, v;
+    int node_from, node_to;
     while (true)
     {
         cout << "Connection (Room A Room B, or -1 -1 to finish): ";
 
-        if (!(cin >> u >> v))
+        if (!(cin >> node_from >> node_to))
         {
             cout << "Not valid format of input. You must enter two numbers." << endl;
             cin.clear();
@@ -39,106 +38,106 @@ vector<vector<int>> inputGraph(int &num_rooms)
             continue;
         }
 
-        if (u == -1 && v == -1)
+        if (node_from == -1 && node_to == -1)
         {
             break;
         }
 
-        if (u < 0 || u >= num_rooms || v < 0 || v >= num_rooms)
+        if (node_from < 0 || node_from >= total_nodes || node_to < 0 || node_to >= total_nodes)
         {
-            cout << "Error: Room numbers must be between 0 and " << num_rooms - 1 << ". Try again." << endl;
+            cout << "Error: Room numbers must be between 0 and " << total_nodes - 1 << ". Try again." << endl;
         }
-        else if (u == v)
+        else if (node_from == node_to)
         {
             cout << "Skipping connection: A room cannot be connected to itself." << endl;
         }
         else
         {
-            if (AM[u][v] == 1)
+            if (layout_Room[node_from][node_to] == 1)
             {
-                cout << "Connection already exists between " << u << " and " << v << ". Skipping." << endl;
+                cout << "Connection already exists between " << node_from << " and " << node_to << ". Skipping." << endl;
             }
             else
             {
-                AM[u][v] = 1;
-                AM[v][u] = 1;
-                cout << "Connection established between Room " << u << " and Room " << v << endl;
+                layout_Room[node_from][node_to] = 1;
+                layout_Room[node_to][node_from] = 1;
+                cout << "Connection established between Room " << node_from << " and Room " << node_to << endl;
             }
         }
     }
 
-    return AM;
+    return layout_Room;
 }
 
-vector<int> Path_Reconstruction(vector<int> &arr, int start, int exit)
+vector<int> reconstructRoute(vector<int> &predecessor_array, int origin, int destination)
 {
-    vector<int> reconstructed_path;
+    vector<int> route_list;
 
-    int current = exit;
-    reconstructed_path.push_back(current);
-    while (arr[current] != -2)
+    int current_node = destination;
+    route_list.push_back(current_node);
+    while (predecessor_array[current_node] != -2)
     {
-        reconstructed_path.push_back(arr[current]);
-        current = arr[current];
+        route_list.push_back(predecessor_array[current_node]);
+        current_node = predecessor_array[current_node];
     }
-    reverse(reconstructed_path.begin(), reconstructed_path.end());
-    return reconstructed_path;
+    reverse(route_list.begin(), route_list.end());
+    return route_list;
 }
 
-void Shortest_Path(vector<vector<int>> &AM, int start, int exit)
+void findOptimalRoute(const vector<vector<int>> &graph_matrix, int origin, int destination)
 {
 
-    vector<int> parent_Arr(AM.size(), -1);
-    parent_Arr[start] = -2;
-    queue<int> visited;
+    vector<int> predecessor_array(graph_matrix.size(), -1);
+    predecessor_array[origin] = -2;
+    queue<int> exploration_queue;
 
-    visited.push(start);
-    while (!visited.empty())
+    exploration_queue.push(origin);
+    while (!exploration_queue.empty())
     {
-        int current = visited.front();
-        visited.pop();
-        if (current == exit)
+        int current_node = exploration_queue.front();
+        exploration_queue.pop();
+        if (current_node == destination)
         {
             cout << "exit path found" << endl;
             break;
         }
-        for (int v = 0; v < AM.size(); v++)
+        for (int nb = 0; nb < graph_matrix.size(); nb++)
         {
-            if (AM[current][v] == 1 && parent_Arr[v] == -1)
+            if (graph_matrix[current_node][nb] == 1 && predecessor_array[nb] == -1)
             {
-                parent_Arr[v] = current;
-                visited.push(v);
+                predecessor_array[nb] = current_node;
+                exploration_queue.push(nb);
             }
         }
     }
-    if (parent_Arr[exit] == -1)
+    if (predecessor_array[destination] == -1)
     {
         cout << "no viable path found" << endl;
         return;
     }
 
-    vector<int> sp = Path_Reconstruction(parent_Arr, start, exit);
-    for (int i = 0; i < sp.size() - 1; i++)
+    vector<int> optimal_route = reconstructRoute(predecessor_array, origin, destination);
+    for (int jkf = 0; jkf < optimal_route.size() - 1; jkf++)
     {
-        cout << sp[i] << "->";
+        cout << optimal_route[jkf] << "->";
     }
-    cout << sp[sp.size() - 1];
+    cout << optimal_route[optimal_route.size() - 1];
     cout << endl;
 }
 
 int main()
 {
-    int num_Rooms;
-    vector<vector<int>> LayoutBuilding = inputGraph(num_Rooms);
+    int total_nodes;
+    vector<vector<int>> building_graph = buildGraphStructure(total_nodes);
 
-    int startRoom;
+    int origin_room;
     cout << "enter starting room" << endl;
-    cin >> startRoom;
-    int exit;
+    cin >> origin_room;
+    int destination_room;
     cout << "enter emergency exit room" << endl;
-    cin >> exit;
+    cin >> destination_room;
 
-    Shortest_Path(LayoutBuilding, startRoom, exit);
+    findOptimalRoute(building_graph, origin_room, destination_room);
 
     return 0;
 }
